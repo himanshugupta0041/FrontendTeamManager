3e3876140ffe070a96b1269284c7fbf2de4889b1
himanshugupta0041 <hg5079424@gmail.com>
2026-05-01 19:07:48 +0530

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,15 @@
-﻿import React from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
 const Layout = () => {
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="flex">
       <Sidebar />
-      <div className="flex-1 flex flex-col overflow-hidden">
+      <div className="flex-1 ml-64">
         <Navbar />
-        <main className="flex-1 overflow-y-auto p-6">
+        <main className="p-6">
           <Outlet />
         </main>
       </div>
