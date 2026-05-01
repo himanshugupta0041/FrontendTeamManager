266a24f552e1798fdaa59c66bc5662dc6c8bf49d
Himanshu <hg5079424@gmail.com>
2026-05-01 17:25:07 +0530

    new change

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
