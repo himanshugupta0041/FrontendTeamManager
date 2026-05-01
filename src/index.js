3c310d3ce6c0645421ebffec247a99c0fdc3daf8
Himanshu <hg5079424@gmail.com>
2026-05-01 17:19:16 +0530

    temo cahnegs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-﻿import { HashRouter } from 'react-router-dom';
-// Instead of BrowserRouter
+﻿import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
